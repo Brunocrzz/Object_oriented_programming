3fd099dc2c0d93258f55d3c7748857a88f62fe5b
Matheus Rodrigues <rmatheus440@gmail.com>
2023-10-30 23:56:50 -0300

    update classes

---
+++ Projeto Detran/src/dados/Dados.java
@@ -0,0 +1,41 @@
+package dados;
+
+import usuario.Usuario;
+import infracao.Infracao;
+import cnh.Cnh;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dados {
+    private List<Usuario> usuarios = new ArrayList<>();
+    private List<Infracao> infracoes = new ArrayList<>();
+    private List<Cnh> cnhs = new ArrayList<>();
+
+    public void addUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public void removeUsuario(Usuario usuario) {
+        usuarios.remove(usuario);
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void addInfracao(Infracao infracao) {
+        infracoes.add(infracao);
+    }
+
+    public List<Infracao> getInfracoes() {
+        return infracoes;
+    }
+
+    public void addCnh(Cnh cnh) {
+        cnhs.add(cnh);
+    }
+
+    public List<Cnh> getCnhs() {
+        return cnhs;
+    }
+}
